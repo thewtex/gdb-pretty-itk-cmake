c605ec809e1d75f5ef1e28e3e2b4748eaca3ce0f
Matt McCormick (thewtex) <matt@mmmccormick.com>
2010-11-21 01:42:27 -0600

    Improve module documentation.

--- GDBPrettyITK.cmake
+++ GDBPrettyITK.cmake
@@ -1,11 +1,26 @@
 # This is a macro to ease the use of gdb-pretty-itk.
-# http://gitorious.org/gdb-pretty/gdb-pretty-itk
-# Passing in a target into the macro will add the CMake magic to generate and
+#
+# See:
+#   http://www.itk.org/Wiki/ITK/GDBPretty
+#   http://gitorious.org/gdb-pretty/gdb-pretty-itk
+#
+# Passing in a target into the function will add the CMake magic to generate and
 # copy the file needed to register ITK specific pretty-printers when debugging
 # with gdb.  This file consists of the pretty-printers in which you are
-# interested.  Feel free to copy this file and the template into your project as
-# needed.
-# See the CMakeLists.txt in the examples/ directory for an example of usage.
+# interested.  The contents of the file are specified with CMake configuration
+# variables.
+#
+# Therefore, if you have something like this in your CMakeLists.txt
+#
+#   add_executable( myexe main.cxx )
+#   target_link_libraries( myexe ITKCommon ITKIO )
+#
+# The following will register the pretty-printers:
+#
+#   GDB_PRETTY_ITK( myexe )
+#
+# IMPORTANT: include this file *after* include( ${ITK_USE_FILE} )
+# so the correct ITK_VERSION_MAJOR is detected.
 function( GDB_PRETTY_ITK target )
   get_target_property( target_location ${target} LOCATION )
   add_custom_command( TARGET ${target}
